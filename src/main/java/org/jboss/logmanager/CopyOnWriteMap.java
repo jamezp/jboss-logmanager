d3c7a55998d312525fa39cd38e30849df0755b38
James R. Perkins <jperkins@redhat.com>
2023-06-27 12:15:27 -0700

    *** Quick and Dirty CopyOnWriteMap replacement.
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- src/main/java/org/jboss/logmanager/CopyOnWriteMap.java
+++ src/main/java/org/jboss/logmanager/CopyOnWriteMap.java
@@ -19,67 +19,173 @@
 
 package org.jboss.logmanager;
 
-import java.util.AbstractMap;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
-final class CopyOnWriteMap<K, V> extends AbstractMap<K, V> implements ConcurrentMap<K, V>, Cloneable {
+final class CopyOnWriteMap<K, V> implements ConcurrentMap<K, V>, Cloneable {
+    private volatile Map<K, V> delegate;
+    private static final VarHandle DELEGATE_HANDLER;
 
-    private static final FastCopyHashMap EMPTY = new FastCopyHashMap(32, 0.25f);
-
-    @SuppressWarnings("unchecked")
-    private volatile FastCopyHashMap<K, V> map = EMPTY;
-
-    private static final AtomicReferenceFieldUpdater<CopyOnWriteMap, FastCopyHashMap> mapUpdater = AtomicReferenceFieldUpdater
-            .newUpdater(CopyOnWriteMap.class, FastCopyHashMap.class, "map");
-
-    public V get(final Object key) {
-        return map.get(key);
+    static {
+        try {
+            DELEGATE_HANDLER = MethodHandles.lookup().findVarHandle(CopyOnWriteMap.class, "delegate", Map.class);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new ExceptionInInitializerError(e);
+        }
     }
 
-    public boolean containsKey(final Object key) {
-        return map.containsKey(key);
+    CopyOnWriteMap() {
+        delegate = Map.of();
     }
 
+    @Override
     public int size() {
-        return map.size();
+        return delegate.size();
     }
 
+    @Override
+    public boolean isEmpty() {
+        return delegate.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(final Object key) {
+        return delegate.containsKey(key);
+    }
+
+    @Override
     public boolean containsValue(final Object value) {
-        return map.containsValue(value);
+        return delegate.containsValue(value);
     }
 
-    @SuppressWarnings("unchecked")
-    public void clear() {
-        map = EMPTY;
+    @Override
+    public V get(final Object key) {
+        return delegate.get(key);
     }
 
+    @Override
     public V put(final K key, final V value) {
-        FastCopyHashMap<K, V> oldVal, newVal;
-        V result;
-        do {
-            oldVal = map;
-            newVal = oldVal.clone();
-            result = newVal.put(key, value);
-        } while (!mapUpdater.compareAndSet(this, oldVal, newVal));
-        return result;
+        final Map<K, V> newMap = new LinkedHashMap<>(delegate);
+        try {
+            return newMap.put(key, value);
+        } finally {
+            DELEGATE_HANDLER.setVolatile(this, Map.copyOf(newMap));
+        }
     }
 
+    @Override
     public V remove(final Object key) {
-        FastCopyHashMap<K, V> oldVal, newVal;
-        V result;
-        do {
-            oldVal = map;
-            if (!oldVal.containsKey(key)) {
-                return null;
-            }
-            newVal = oldVal.clone();
-            result = newVal.remove(key);
-        } while (!mapUpdater.compareAndSet(this, oldVal, newVal));
-        return result;
+        final Map<K, V> newMap = new LinkedHashMap<>(delegate);
+        try {
+            return newMap.remove(key);
+        } finally {
+            DELEGATE_HANDLER.setVolatile(this, Map.copyOf(newMap));
+        }
+    }
+
+    @Override
+    public void putAll(final Map<? extends K, ? extends V> m) {
+        final Map<K, V> newMap = new LinkedHashMap<>(delegate);
+        newMap.putAll(m);
+        DELEGATE_HANDLER.setVolatile(this, Map.copyOf(newMap));
+    }
+
+    @Override
+    public void clear() {
+        DELEGATE_HANDLER.setVolatile(this, Map.of());
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return delegate.keySet();
+    }
+
+    @Override
+    public Collection<V> values() {
+        return delegate.values();
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return delegate.entrySet();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return delegate.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return delegate.hashCode();
+    }
+
+    @Override
+    public V getOrDefault(final Object key, final V defaultValue) {
+        return delegate.getOrDefault(key, defaultValue);
+    }
+
+    @Override
+    public void forEach(final BiConsumer<? super K, ? super V> action) {
+        delegate.forEach(action);
+    }
+
+    @Override
+    public void replaceAll(final BiFunction<? super K, ? super V, ? extends V> function) {
+        delegate.replaceAll(function);
+    }
+
+    @Override
+    public V putIfAbsent(final K key, final V value) {
+        final Map<K, V> newMap = new LinkedHashMap<>(delegate);
+        try {
+            return newMap.putIfAbsent(key, value);
+        } finally {
+            DELEGATE_HANDLER.setVolatile(this, Map.copyOf(newMap));
+        }
+    }
+
+    @Override
+    public boolean remove(final Object key, final Object value) {
+        return delegate.remove(key, value);
+    }
+
+    @Override
+    public boolean replace(final K key, final V oldValue, final V newValue) {
+        return delegate.replace(key, oldValue, newValue);
+    }
+
+    @Override
+    public V replace(final K key, final V value) {
+        return delegate.replace(key, value);
+    }
+
+    @Override
+    public V computeIfAbsent(K key, Function<? super K, ? extends V> mappingFunction) {
+        return delegate.computeIfAbsent(key, mappingFunction);
+    }
+
+    @Override
+    public V computeIfPresent(final K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+        return delegate.computeIfPresent(key, remappingFunction);
+    }
+
+    @Override
+    public V compute(final K key, final BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+        return delegate.compute(key, remappingFunction);
+    }
+
+    @Override
+    public V merge(final K key, final V value, final BiFunction<? super V, ? super V, ? extends V> remappingFunction) {
+        return delegate.merge(key, value, remappingFunction);
     }
 
     @SuppressWarnings("unchecked")
@@ -89,78 +195,5 @@ final class CopyOnWriteMap<K, V> extends AbstractMap<K, V> implements Concurrent
         } catch (CloneNotSupportedException e) {
             throw new IllegalStateException();
         }
-    }
-
-    public V putIfAbsent(final K key, final V value) {
-        FastCopyHashMap<K, V> oldVal, newVal;
-        do {
-            oldVal = map;
-            if (oldVal.containsKey(key)) {
-                return oldVal.get(key);
-            }
-            newVal = oldVal.clone();
-            newVal.put(key, value);
-        } while (!mapUpdater.compareAndSet(this, oldVal, newVal));
-        return null;
-    }
-
-    @SuppressWarnings("unchecked")
-    public boolean remove(final Object key, final Object value) {
-        FastCopyHashMap<K, V> oldVal, newVal;
-        do {
-            oldVal = map;
-            if (value == oldVal.get(key) && (value != null || oldVal.containsKey(key))) {
-                if (oldVal.size() == 1) {
-                    newVal = EMPTY;
-                } else {
-                    newVal = oldVal.clone();
-                    newVal.remove(key);
-                }
-            } else {
-                return false;
-            }
-        } while (!mapUpdater.compareAndSet(this, oldVal, newVal));
-        return true;
-    }
-
-    public boolean replace(final K key, final V oldValue, final V newValue) {
-        FastCopyHashMap<K, V> oldVal, newVal;
-        do {
-            oldVal = map;
-            if (oldValue == oldVal.get(key) && (oldValue != null || oldVal.containsKey(key))) {
-                newVal = oldVal.clone();
-                newVal.put(key, newValue);
-            } else {
-                return false;
-            }
-        } while (!mapUpdater.compareAndSet(this, oldVal, newVal));
-        return true;
-    }
-
-    public V replace(final K key, final V value) {
-        FastCopyHashMap<K, V> oldVal, newVal;
-        V result;
-        do {
-            oldVal = map;
-            if (value == oldVal.get(key) && (value != null || oldVal.containsKey(key))) {
-                newVal = oldVal.clone();
-                result = newVal.put(key, value);
-            } else {
-                return null;
-            }
-        } while (!mapUpdater.compareAndSet(this, oldVal, newVal));
-        return result;
-    }
-
-    public Set<K> keySet() {
-        return Collections.unmodifiableSet(map.keySet());
-    }
-
-    public Collection<V> values() {
-        return Collections.unmodifiableCollection(map.values());
-    }
-
-    public Set<Entry<K, V>> entrySet() {
-        return Collections.unmodifiableSet(map.entrySet());
     }
 }
